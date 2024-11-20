43c4fe8acdad4fa9786e81c04b9e0d4c83a481cc
dcoderk <webmarket2k@gmail.com>
2024-11-20 19:10:29 +0800

    creating layout components and adding vite

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Postiions</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    
+</body>
+</html>
