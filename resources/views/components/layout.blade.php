60a860a463927b8264bed789b9b5ea6492927ade
dcoderk <webmarket2k@gmail.com>
2024-12-11 22:54:37 +0800

    auth and search features added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
 
     <div class="px-10">
         <nav class="flex justify-between item-center py-4 border-b border-white/10">
@@ -23,9 +23,20 @@
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
             </div>
-            <div>
-                <a href="#">Post a Job</a>
-            </div>
+
+            @auth
+                <div>
+                    <a href="/jobs/create">Post a Job</a>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            @endguest
+            
         </nav>
 
         <main class="mt-10 max-w-[986px] mx-auto">
