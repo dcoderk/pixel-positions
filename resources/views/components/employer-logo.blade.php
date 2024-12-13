5beee6352325acb90b64a7ed7c306b057ccdeead
dcoderk <webmarket2k@gmail.com>
2024-12-13 23:27:25 +0800

    adding asset logo

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,9 +1,3 @@
-@props(['width' => 90])
+@props(['employer', 'width' => 90])
 
-<img src="https://picsum.photos/seed/{{ rand(0,10000) }}/{{ $width }}" alt="" class="rounded-xl">
-
-
-{{-- @props(['employer', 'width' => 90])
-
-<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}"> --}}  
-{{-- not loading the placeholder logo --}}
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}"> 
