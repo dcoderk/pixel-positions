684e4e60e3e48018843c5ddd4f69cf0978169bc7
dcoderk <webmarket2k@gmail.com>
2024-12-13 23:04:01 +0800

    create job, search and optimize query

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,8 +9,11 @@ class SearchController extends Controller
 {
     public function __invoke()
     {
-        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
-
+        $jobs = Job::query()
+            ->with(['employer', 'tags'])
+            ->where('title', 'LIKE', '%'.request('q').'%')
+            ->get();
+    
         return view('results', ['jobs' => $jobs]);
     }
 }
